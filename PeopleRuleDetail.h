3586598842e93b016500607c93bd94489cdaafc1
andy <hznhappy@163.com>
2012-02-15 18:52:06 +0800

    add untrack file

---
+++ PeopleRuleDetail.h
@@ -0,0 +1,22 @@
+//
+//  PeopleRuleDetail.h
+//  PhotoApp
+//
+//  Created by apple on 2/15/12.
+//  Copyright (c) 2012 chinarewards. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class People, PeopleRule;
+
+@interface PeopleRuleDetail : NSManagedObject
+
+@property (nonatomic, retain) NSString * firstName;
+@property (nonatomic, retain) NSString * lastName;
+@property (nonatomic, retain) NSString * opcode;
+@property (nonatomic, retain) People *conPeople;
+@property (nonatomic, retain) PeopleRule *conPeopleRule;
+
+@end
