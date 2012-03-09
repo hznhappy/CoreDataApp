625a363ab5a203ae423f7cca609f6da5b599b891
andy <hznhappy@163.com>
2012-03-09 18:56:28 +0800

    push

--- AssetTablePicker.m
+++ AssetTablePicker.m
@@ -1022,14 +1022,14 @@
     
     array = nil;
     array = [[NSArray alloc]init];
-    array=[array arrayByAddingObjectsFromArray:recentTwoWk];
-    
-    array=[array arrayByAddingObjectsFromArray:twoWkToOneMth];
-    array=[array arrayByAddingObjectsFromArray:oneToThreeMth];
-    array=[array arrayByAddingObjectsFromArray:threeToSixMth];
-    array=[array arrayByAddingObjectsFromArray:sixMthToOneYear];
-    array=[array arrayByAddingObjectsFromArray:moreThanOneYear];
     array=[array arrayByAddingObjectsFromArray:unknowDate];
+    
+    array=[array arrayByAddingObjectsFromArray:moreThanOneYear];
+    array=[array arrayByAddingObjectsFromArray:sixMthToOneYear];
+    array=[array arrayByAddingObjectsFromArray:threeToSixMth];
+    array=[array arrayByAddingObjectsFromArray:oneToThreeMth];
+    array=[array arrayByAddingObjectsFromArray:twoWkToOneMth];
+    array=[array arrayByAddingObjectsFromArray:recentTwoWk];
     return array;
     
 }
@@ -1047,31 +1047,19 @@
         videoType = NO;
         [self setTimeSelectionWithIndex:timeSelectionAll];
        allTableData = [self dataInUITableViewSectionsFromArray:allTableData];
-//        if (timeSelectionAll != 0) {
-//            timeFilter = YES;
-//        }else{
-//            timeFilter = NO;
-//        }
+
     }else if(seg.selectedSegmentIndex == 1){
         photoType = YES;
         videoType = NO;
         [self setTimeSelectionWithIndex:timeSelectionPhoto];
       photoTableData = [self dataInUITableViewSectionsFromArray:photoTableData];
-//        if (timeSelectionPhoto != 0) {
-//            timeFilter = YES;
-//        }else{
-//            timeFilter = NO;
-//        }
+
     }else{
         photoType = NO;
         videoType = YES;
         [self setTimeSelectionWithIndex:timeSelectionVideo];
        videoTableData = [self dataInUITableViewSectionsFromArray:videoTableData];
-//        if (timeSelectionVideo != 0) {
-//            timeFilter = YES;
-//        }else{
-//            timeFilter = NO;
-//        }
+
     }
     [self.table reloadData];
     NSIndexPath* ip = nil;
@@ -1765,13 +1753,13 @@
     } else if (scrollView.contentOffset.y>=sectionHeaderHeight) {
         scrollView.contentInset = UIEdgeInsetsMake(-sectionHeaderHeight, 0, 0, 0);
     }
-    CGPoint offset = scrollView.contentOffset;
-    CGRect bounds = scrollView.bounds;
-    CGSize size = scrollView.contentSize;
+   // CGPoint offset = scrollView.contentOffset;
+    //CGRect bounds = scrollView.bounds;
+    //CGSize size = scrollView.contentSize;
     UIEdgeInsets insets = scrollView.contentInset;
-    float y = offset.y + bounds.size.height - insets.bottom;
-    float h = size.height;
-    if (insets.top  > -40) {
+    //float y = offset.y + bounds.size.height - insets.bottom;
+    //float h = size.height;
+    if (insets.top  > -40 && !scrollView.dragging) {
         [self setTableViewEdge:oritation];
     }
     }
@@ -1795,12 +1783,7 @@
 
 }
 
--(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
-//    UIEdgeInsets insets = scrollView.contentInset;
-//    if (insets.top  > -50) {
-//        [self setTableViewEdge:oritation];
-//    }
-}
+
 #pragma  mark -
 #pragma  mark AlerView delegate method
 -(void)alertView:(UIAlertView *)alert11 didDismissWithButtonIndex:(NSInteger)buttonIndex{
